d7a816625c1daeb868a1533c1d656e783c0e2b3e
Kenny Noverian <kennynoverian@gmail.com>
2024-10-12 04:43:13 +0200

    changing id button value

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-const btn = document.querySelector("#btn");
+const btn = document.querySelector("#button");
 const h2 = document.querySelector("h2");
 
+button.addEventListener('click', () => {
+    const newColor = randomColorGen();
+    document.body.style.backgroundColor = newColor;
+    h2.innerText = newColor;
+})
 
 const randomColorGen = () => {
     const r = Math.floor(Math.random() * 255);
